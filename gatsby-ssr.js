ce53a1c5a9a0ce7d01db2cb3bd7b1f47f08a16e1
o.baaziz <o.baaziz@pharmainvest.dz>
2021-03-06 17:53:53 +0100

    changed to backtick

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,7 @@ import React from "react"
 export const onRenderBody = ({ setPostBodyComponents }, pluginOptions) => {
   setPostBodyComponents([
     <script type="text/javascript">
-      var subscribersSiteId='927cba08-e8f0-48b4-989a-96000c1dfb84';
+      var subscribersSiteId=`927cba08-e8f0-48b4-989a-96000c1dfb84`;
     </script>,
     <script
       type="text/javascript"
