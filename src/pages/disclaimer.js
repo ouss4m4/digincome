7280f6b19ba342dc6005c85f9fa2144f1b120de7
oussama <bz.oussama@gmail.com>
2021-03-07 23:29:02 +0100

    added policies

---
+++ src/pages/disclaimer.js
@@ -0,0 +1,113 @@
+import React from "react"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const DisclaimerPage = ({ location }) => (
+  <Layout location={location} title="Disclaimer">
+    <SEO title="Disclaimer" />
+    <h3 id="terms-conditions">Terms &amp; Conditions</h3>
+    <ol>
+      <li>
+        <p>
+          Introduction These terms and conditions govern your use of our
+          website; by using our website, you accept these terms and conditions
+          in full. If you disagree with any part of these terms and conditions,
+          do not use our website.
+        </p>
+      </li>
+      <li>
+        <p>
+          Intellectual property rights Unless otherwise stated, we or our
+          licensors own the intellectual property rights in the website and
+          material on the website. Subject to the licence below, all our
+          intellectual property rights are reserved.
+        </p>
+      </li>
+      <li>
+        <p>
+          Licence to use website You may view, download for caching purposes
+          only, and print pages from the website, provided that:
+        </p>
+        <ul>
+          <li>
+            you must not republish material from this website (including
+            republication on another website), or reproduce or store material
+            from this website in any public or private electronic retrieval
+            system;
+          </li>
+          <li>
+            you must not reproduce, duplicate, copy, sell, resell, visit, or
+            otherwise exploit our website or material on our website for a
+            commercial purpose, without our express written consent;
+          </li>
+          <li>
+            you must not edit or otherwise modify any material on the website.
+          </li>
+        </ul>
+      </li>
+      <li>
+        <p>
+          Limitations of liability Some of the information on this website is
+          provided free-of-charge, and you acknowledge that it would be
+          unreasonable to hold us liable in respect of this website and the
+          information on this website. Whilst we endeavour to ensure that the
+          information on this website is correct, we do not warrant its
+          completeness or accuracy; nor do we not commit to ensuring that the
+          website remains available or that the material on this website is kept
+          up-to-date. To the maximum extent permitted by applicable law we
+          exclude all representations, warranties and conditions (including,
+          without limitation, the conditions implied by law of satisfactory
+          quality, fitness for purpose and the use of reasonable care and
+          skill). Our liability is limited and excluded to the maximum extent
+          permitted under applicable law. We will not be liable for any direct,
+          indirect or consequential loss or damage arising under these terms and
+          conditions or in connection with our website, whether arising in tort,
+          contract, or otherwise – including, without limitation, any loss of
+          profit, contracts, business, goodwill, data, income, revenue or
+          anticipated savings. However, nothing in these terms and conditions
+          shall exclude or limit our liability for fraud, for death or personal
+          injury caused by our negligence, or for any other liability which
+          cannot be excluded or limited under applicable law.
+        </p>
+      </li>
+      <li>
+        <p>
+          Restricted access Access to certain areas of our website is
+          restricted. We reserve the right to restrict access to other areas of
+          our website, or indeed our whole website, at our discretion. If we
+          provide you with a user ID and password to enable you to access
+          restricted areas of our website or other content or services, you must
+          ensure that that user ID and password is kept confidential. You accept
+          responsibility for all activities that occur under your user ID or
+          password. We may disable your user ID and password at our sole
+          discretion or if you breach any of the policies or terms governing
+          your use of our website or any other contractual obligation you owe to
+          us.
+        </p>
+      </li>
+      <li>
+        <p>
+          Variation We may revise these terms and conditions from time-to-time.
+          Please check this page regularly to ensure you are familiar with the
+          current version.
+        </p>
+      </li>
+      <li>
+        Entire agreement These terms and conditions, together with our privacy
+        policy, constitute the entire agreement between you and us in relation
+        to your use of our website, and supersede all previous agreements in
+        respect of your use of this website.
+      </li>
+      <li>
+        Law and jurisdiction This notice will be governed by and construed in
+        accordance with English law, and any disputes relating to this notice
+        shall be subject to the exclusive jurisdiction of the courts of England.
+      </li>
+      <li>
+        Our contact details You can contact us at incomedig@gmail.com
+      </li>
+    </ol>
+  </Layout>
+)
+
+export default DisclaimerPage
