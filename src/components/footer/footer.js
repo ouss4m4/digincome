451ea2d9a3f69bdf0d64218d496de012958e679a
o.baaziz <o.baaziz@pharmainvest.dz>
2021-03-10 11:03:21 +0100

    removed errors

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react"
+import React from "react"
 /* import addToMailchimp from "gatsby-plugin-mailchimp" */
 
 /* import SubscriptionModal from "../subs-modal/SubscriptionModal" */
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,34 +1,15 @@
 import React from "react"
-import { Link } from "gatsby"
 import Header from "./header"
 import Footer from "./footer/footer"
 
-const Layout = ({ location, title, children }) => {
-  const rootPath = `${__PATH_PREFIX__}/`
-  const isRootPath = location.pathname === rootPath
-  let header
-
-  if (isRootPath) {
-    header = (
-      <h1 className="main-heading">
-        <Link to="/">{title}</Link>
-      </h1>
-    )
-  } else {
-    header = (
-      <Link className="header-link-home" to="/">
-        {title}
-      </Link>
-    )
-  }
-
+const Layout = ({ children }) => {
   return (
     <div>
       <Header />
-      <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <div className="global-wrapper">
         <main>{children}</main>
       </div>
-     <Footer />
+      <Footer />
     </div>
   )
 }
