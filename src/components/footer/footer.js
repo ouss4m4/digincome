66cf7191709c6ceedb542730db3a48d1cd483696
oussama <bz.oussama@gmail.com>
2021-03-12 23:06:53 +0100

    footer set

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -53,99 +53,57 @@ export default function Footer() {
   return (
     <footer className="footer">
       {/*  {showModal && <SubscriptionModal closeModal={closeModal} />} */}
-      <div className="limit-size">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="col-sm-12 form-wrap">
-              <p>
-                {" "}
-                Subscribe to our news Letters and get weekly Digital Income news{" "}
-              </p>
-              <form onSubmit={_handleSubmit}>
-                <input
-                  type="text"
-                  onChange={handleNameChange}
-                  value={formName}
-                  placeholder="name"
-                  name="name"
-                  className="name-input"
-                />
-                <input
-                  type="email"
-                  onChange={handleMailChange}
-                  value={formEmail}
-                  placeholder="email"
-                  name="email"
-                  className="email-input"
-                  required
-                />
-                <input className="submit-btn" type="submit" value="Subscribe" />
-                {/* {isPosting ? (
-                  <svg className="spinner" viewBox="0 0 50 50">
-                    <circle
-                      className="path"
-                      cx="25"
-                      cy="25"
-                      r="20"
-                      fill="none"
-                      strokeWidth="5"
-                    ></circle>
-                  </svg>
-                ) : (
-                  <input
-                    className="submit-btn"
-                    type="submit"
-                    value="Subscribe"
-                  />
-                )} */}
-              </form>
-            </div>
-          </div>
 
-          <div className="col-sm-8">
-            <div className="links">
-              <p>Copyright © 2021 </p> <p> Digital Income</p>{" "}
-              <a style={{ boxShadow: "none" }} href="/privacy">
-                Privacy policy
-              </a>
-              <a style={{ boxShadow: "none" }} href="/affiliate">
-                Affiliate policy
-              </a>
-              <a style={{ boxShadow: "none" }} href="/disclaimer">
-                Disclaimer
-              </a>
-              <a style={{ boxShadow: "none" }} href="/sitemap.xml">
-                Sitemap
-              </a>
-            </div>
-          </div>
-          <div className="col-sm-4">
-            <div className="sharewrap">
-              <Share
-                iconSize={34}
-                margin={"6px"}
-                media={
-                  "https://digincome.com/static/c7a038fa28d293ce55ebda088bfe48b3/63d84/Stonks.png"
-                }
-                round={true}
-                shareUrl={"https://digincome.com"}
-                title={"Digital Income"}
+      <div className="container">
+        <div className="upper">
+          <div className="form-wrap">
+            <p>
+              Subscribe to our Newsletter and get weekly updates and Digital
+              Market News to help you grow.
+            </p>
+            <form onSubmit={_handleSubmit}>
+              <input
+                type="text"
+                onChange={handleNameChange}
+                value={formName}
+                placeholder="name"
+                name="name"
+                className="name-input"
               />
-            </div>
+              <input
+                type="email"
+                onChange={handleMailChange}
+                value={formEmail}
+                placeholder="email"
+                name="email"
+                className="email-input"
+                required
+              />
+              <input className="submit-btn" type="submit" value="Subscribe" />
+            </form>
           </div>
-          <div className="col-sm-12">
-            <blockquote style={{ fontSize: "0.8em", margin: "0" }}>
-              Any information provided on this website does not constitute
-              investment advice or investment recommendation nor does it
-              constitute an offer to buy or sell or a solicitation of an offer
-              to buy or sell shares or units in any of the investment funds or
-              other financial instruments described on this website.
-            </blockquote>
+          <div className="sharewrap">
+            <Share
+              iconSize={34}
+              margin={"6px"}
+              media={
+                "https://digincome.com/static/c7a038fa28d293ce55ebda088bfe48b3/63d84/Stonks.png"
+              }
+              round={true}
+              shareUrl={"https://digincome.com"}
+              title={"Digital Income"}
+            />
           </div>
-
-          {/*
-           */}
         </div>
+        <blockquote
+          style={{ fontSize: "0.8em", margin: "0", maxWidth: "800px" }}
+        >
+          Any information provided on this website does not constitute
+          investment advice or investment recommendation nor does it constitute
+          an offer to buy or sell or a solicitation of an offer to buy or sell
+          shares or units in any of the investment funds or other financial
+          instruments described on this website.
+        </blockquote>
       </div>
     </footer>
   )
