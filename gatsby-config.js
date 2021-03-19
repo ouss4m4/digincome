7248792418b17e477e44751a1cf33b420dda5270
o.baaziz <o.baaziz@pharmainvest.dz>
2021-03-19 18:37:50 +0100

    added canonicals and robots

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://digincome.com`,
+        stripQueryString: true,
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
